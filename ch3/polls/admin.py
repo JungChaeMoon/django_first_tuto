b0c07086256451ad4b230995fd6d244cdd18bbfa
ChaeMoon <jungchaemoon@gmail.com>
2019-02-19 18:41:05 +0900

    update datafields

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -11,6 +11,9 @@ from polls.models import Question, Choice
 from django.contrib import admin
 from polls.models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 
-admin.site.register(Question)
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
