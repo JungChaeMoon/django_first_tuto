7cf5de1e9b115381d4268566e39a7b4de3d05466
ChaeMoon <jungchaemoon@gmail.com>
2019-02-20 16:52:13 +0900

    update admin page

--- ch3/books/admin.py
+++ ch3/books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django import admin
+from books.models import Book,Author,Publisher
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Publisher)
+
--- ch3/books/models.py
+++ ch3/books/models.py
@@ -26,4 +26,3 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
-    
