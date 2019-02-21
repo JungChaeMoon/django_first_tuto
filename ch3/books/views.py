005147d42d82f95465b0e6be450dd2e697b7cee9
ChaeMoon <jungchaemoon@gmail.com>
2019-02-21 14:49:53 +0900

    update root page

--- ch3/books/views.py
+++ ch3/books/views.py
@@ -13,12 +13,7 @@ class BooksModelView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #context['model_list'] = ['Book', 'Author', 'Publisher']
-        dictVerbose={}
-        for app in apps.get_app_configs():
-            if 'site-packages' not in app.path:
-                dictVerbose[app.label] = app.verbose_name
-        context['verbose_dict'] = dictVerbose
+        context['model_list'] = ['Book', 'Author', 'Publisher']
         return context
 
 class BookList(ListView):
