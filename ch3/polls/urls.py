5c0d1f5decf57d9781121cd917e114fc0c9cb0db
ChaeMoon <jungchaemoon@gmail.com>
2019-02-22 16:35:01 +0900

    update CBV

--- ch3/polls/urls.py
+++ ch3/polls/urls.py
@@ -3,13 +3,21 @@ from . import views
 
 app_name = 'polls'
 
+
+urlpatterns = [
+    path('',views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+"""
 urlpatterns=[
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
+"""
 
 """
 question_id i는 models  에서 declare 되었는데 why안넘어갈까
--- ch3/polls/views.py
+++ ch3/polls/views.py
@@ -5,6 +5,44 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from polls.models import Choice, Question
+from django.views.generic.base import RedirectView
+from django.views.generic import ListView,DetailView
+
+
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(DetailView):  #특정 개체하나를 컨텍스트변수에 담아서 넘겨줌 근데 그변수가 pk로 조회하는경우 그냥 모델클래스만 지정해주면됨
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+             'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+"""
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]   #question 테이블 객체에서 질문이 생성된 역순으로다가 5개 설정
@@ -34,3 +72,4 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+"""
